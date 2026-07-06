da85f71e2d1819e3d9458f1751757b64078b07eb
Cris0420 <fansames@gmail.com>
2026-07-06 00:41:54 -0600

    events.js

---
+++ src/core/events.js
@@ -0,0 +1,43 @@
+// ======================================================
+// Event Manager
+// ======================================================
+
+class Events {
+
+    constructor(){
+
+        this.events={};
+
+    }
+
+    on(event,callback){
+
+        if(!this.events[event]){
+
+            this.events[event]=[];
+
+        }
+
+        this.events[event].push(callback);
+
+    }
+
+    emit(event,data){
+
+        if(!this.events[event]){
+
+            return;
+
+        }
+
+        this.events[event].forEach(callback=>{
+
+            callback(data);
+
+        });
+
+    }
+
+}
+
+export default new Events();
